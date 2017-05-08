f1ff62af0d4206578eb5fdd212a53949b1128398
korbie <mattcorbin@gmail.com>
2017-05-08 07:52:00 +0300

    Second commit

--- preps.js
+++ preps.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Y\'all come back soon now, hear?')
+  res.send('Y\'all come back soon now, hear? This is my updated app.')
 })
 
 app.listen(3000, function () {
